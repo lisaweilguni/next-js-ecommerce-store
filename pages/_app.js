622dfaed60e7453f663b88a0ba11b773bffd9e14
lisaweilguni <lisaweilguni@gmx.at>
2022-09-27 16:16:20 +0200

    Add layout and global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-
+import { css, Global } from '@emotion/react';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 0;
+            font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI,
+              Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+              Helvetica Neue, sans-serif;
+          }
+          * {
+            box-sizing: border-box;
+          }
+        `}
+      />
+      <Layout>
+        {/*
+          The "Component" component refers to
+          the current page that is being rendered
+        */}
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,4 @@
-import { css } from '@emotion/react';
 import Head from 'next/head';
-import Link from 'next/link';
-
-const navStyles = css`
-  background-color: blue;
-  margin-top: 20px;
-  padding: 10px;
-`;
 
 export default function Home() {
   return (
@@ -16,13 +8,7 @@ export default function Home() {
         <meta name="description" content="Overview of the store" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <nav className={navStyles}>
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/products">Products</Link>
-        </nav>
-      </div>
+      <h1>Home</h1>
     </div>
   );
 }
