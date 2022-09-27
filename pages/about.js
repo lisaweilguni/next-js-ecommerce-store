abb7f82334529c21e6bd80824e75ea0b09b4ed6a
lisaweilguni <lisaweilguni@gmx.at>
2022-09-27 15:51:09 +0200

    Add emotion

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+        <meta name="description" content="Information about the creator" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>About</div>
+    </div>
+  );
+}
